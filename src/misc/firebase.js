9f978e3cf4ecb638d011a518339105b6f2592efd
Akif Ahmed <akifahmed90@gmail.com>
2022-09-20 21:40:19 +0530

    initialized firebase

---
+++ src/misc/firebase.js
@@ -0,0 +1,12 @@
+import firebase from 'firebase/app';
+
+const config = {
+  apiKey: 'AIzaSyDPjSNiTR5b2qGc7LN2rG30cWuTw2LWvGI',
+  authDomain: 'chat-web-app-58910.ficrebaseapp.com',
+  projectId: 'chat-web-app-58910',
+  storageBucket: 'chat-web-app-58910.appspot.com',
+  messagingSenderId: '243101179200',
+  appId: '1:243101179200:web:0697a3dc9acc2b789faea5',
+};
+
+const app = firebase.initializeApp(config);
