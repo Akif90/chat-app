31d24c7b3ca56f357bbda0f67fcfb60fd7fcde2e
Akif Ahmed <akifahmed90@gmail.com>
2022-09-24 01:04:39 +0530

    added g/f sign in

--- src/misc/firebase.js
+++ src/misc/firebase.js
@@ -1,12 +1,18 @@
-import firebase from 'firebase/app';
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/database';
 
 const config = {
   apiKey: 'AIzaSyDPjSNiTR5b2qGc7LN2rG30cWuTw2LWvGI',
-  authDomain: 'chat-web-app-58910.ficrebaseapp.com',
+  authDomain: 'chat-web-app-58910.firebaseapp.com',
   projectId: 'chat-web-app-58910',
   storageBucket: 'chat-web-app-58910.appspot.com',
+  databaseURL:
+    'https://chat-web-app-58910-default-rtdb.asia-southeast1.firebasedatabase.app',
   messagingSenderId: '243101179200',
   appId: '1:243101179200:web:0697a3dc9acc2b789faea5',
 };
 
 const app = firebase.initializeApp(config);
+export const auth = app.auth();
+export const database = app.database();
+// interact with the database with this obj
--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -1,7 +1,66 @@
 import React from 'react';
+import { Alert, Button, Col, Container, Grid, Icon, Panel, Row } from 'rsuite';
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/auth';
+import 'firebase/compat/firestore';
+import { auth, database } from '../misc/firebase';
+import '../styles/utility.scss';
 
 const SignIn = () => {
-  return <div>SignIn</div>;
+  // Handler for button onClick
+  // a third func is used which will be called inside
+  // google and facebook handler.
+
+  const signInWithProvider = async provider => {
+    try {
+      const { additionalUserInfo, user } = await auth.signInWithPopup(provider);
+      console.log(additionalUserInfo, user);
+      if (additionalUserInfo.isNewUser) {
+        await database.ref(`/profiles/${user.uid}`).set({
+          name: user.displayName,
+          createdAt: firebase.database.ServerValue.TIMESTAMP,
+        });
+      }
+
+      Alert.success('Signed In', 4000);
+    } catch (err) {
+      Alert.info(err.message, 4000);
+    }
+    // console.log(res);
+  };
+
+  const onFacebookSignIn = () => {
+    signInWithProvider(new firebase.auth.FacebookAuthProvider());
+    // returns an auth obj which will be used in signIn func
+  };
+  const onGoogleSignIn = () => {
+    signInWithProvider(new firebase.auth.GoogleAuthProvider());
+  };
+
+  return (
+    <Container>
+      <Grid className="mt-page">
+        <Row>
+          <Col xs={24} md={12} mdOffset={6}>
+            <Panel>
+              <div className="text-center">
+                <h2>Welcome to Chat</h2>
+                <p>Progressive chat app for genz</p>
+              </div>
+              <div className="mt-3">
+                <Button block color="blue" onClick={onFacebookSignIn}>
+                  <Icon icon="facebook" /> Continue with Facebook
+                </Button>
+                <Button block color="red" onClick={onGoogleSignIn}>
+                  <Icon icon="google" /> Continue with Google
+                </Button>
+              </div>
+            </Panel>
+          </Col>
+        </Row>
+      </Grid>
+    </Container>
+  );
 };
 
 export default SignIn;
