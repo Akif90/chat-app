a36d3f660eb1c3c8d1a094dd17f444ddae39d68d
Akif Ahmed <akifahmed90@gmail.com>
2022-09-29 18:05:31 +0530

    Made drawer responsive

--- src/components/dashboard/DashBoardToggle.jsx
+++ src/components/dashboard/DashBoardToggle.jsx
@@ -1,17 +1,19 @@
 import React from 'react';
 import { Button, Drawer, Icon } from 'rsuite';
 import DashBoard from '.';
-import { useModalState } from '../../misc/custom-hooks';
+import { useMediaQuery, useModalState } from '../../misc/custom-hooks';
 
 const DashBoardToggle = () => {
   const { isOpen, open, close } = useModalState();
-
+  const isMobile = useMediaQuery('(max-width:800px'); // it accepts a media breaking point
+  // isMobile is a boolean var, it is true if we are at the particular max-width
+  // otherwise it is false
   return (
     <>
       <Button block color="blue" onClick={open}>
         <Icon icon="dashboard">Dashboard</Icon>
       </Button>
-      <Drawer show={isOpen} onHide={close} placement="left">
+      <Drawer full={isMobile} show={isOpen} onHide={close} placement="left">
         <DashBoard />
       </Drawer>
     </>
