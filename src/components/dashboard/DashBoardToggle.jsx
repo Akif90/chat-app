e7b54070c86def9745ce8ae0c44b97e64a94d393
Akif Ahmed <akifahmed90@gmail.com>
2022-09-29 19:39:37 +0530

    Added signout functionality

--- src/components/dashboard/DashBoardToggle.jsx
+++ src/components/dashboard/DashBoardToggle.jsx
@@ -1,20 +1,27 @@
-import React from 'react';
-import { Button, Drawer, Icon } from 'rsuite';
+import React, { useCallback } from 'react';
+import { Alert, Button, Drawer, Icon } from 'rsuite';
 import DashBoard from '.';
 import { useMediaQuery, useModalState } from '../../misc/custom-hooks';
+import { auth } from '../../misc/firebase';
 
 const DashBoardToggle = () => {
   const { isOpen, open, close } = useModalState();
   const isMobile = useMediaQuery('(max-width:800px'); // it accepts a media breaking point
   // isMobile is a boolean var, it is true if we are at the particular max-width
   // otherwise it is false
+
+  const onSignOut = useCallback(() => {
+    auth.signOut();
+    Alert.info('Signed Out', 4000);
+    close();
+  }, [close]);
   return (
     <>
       <Button block color="blue" onClick={open}>
         <Icon icon="dashboard">Dashboard</Icon>
       </Button>
       <Drawer full={isMobile} show={isOpen} onHide={close} placement="left">
-        <DashBoard />
+        <DashBoard onSignOut={onSignOut} />
       </Drawer>
     </>
   );
--- src/components/dashboard/index.jsx
+++ src/components/dashboard/index.jsx
@@ -1,7 +1,26 @@
 import React from 'react';
+import { Button, Drawer } from 'rsuite';
+import { useProfile } from '../../context/profile.context';
 
-const DashBoard = () => {
-  return <div>Hello</div>;
+const DashBoard = ({ onSignOut }) => {
+  const { profile } = useProfile();
+  return (
+    <>
+      <Drawer.Header>
+        <Drawer.Title>Dashboard</Drawer.Title>
+      </Drawer.Header>
+
+      <Drawer.Body>
+        <h3> This is {profile.name}</h3>
+      </Drawer.Body>
+
+      <Drawer.Footer>
+        <Button block color="red" onClick={onSignOut}>
+          Sign out
+        </Button>
+      </Drawer.Footer>
+    </>
+  );
 };
 // we have used the index.js name so that we can import
 // just by calling the folder instead of the file
