8742fbd6533450564a443d96b1c1e73b1b61b858
Akif Ahmed <akifahmed90@gmail.com>
2022-10-10 17:16:19 +0530

    created room context

---
+++ src/context/room.context.js
@@ -0,0 +1,25 @@
+import React, { createContext, useState, useEffect } from 'react';
+import { database } from '../misc/firebase';
+import { tranformToArrWithId } from '../misc/helper';
+
+const RoomsContext = createContext();
+export const RoomsProvider = ({ children }) => {
+  const [rooms, setRooms] = useState(null);
+  useEffect(() => {
+    const roomListRef = database.ref('rooms');
+
+    roomListRef.on('value', snap => {
+      const data = tranformToArrWithId(snap.val());
+      setRooms(data);
+      console.log(data);
+
+      return () => {
+        roomListRef.off();
+      };
+    });
+  });
+
+  return (
+    <RoomsContext.Provider value={rooms}>{children}</RoomsContext.Provider>
+  );
+};
